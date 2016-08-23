a7a7f3cae0cbd9591ed6c43382c62a1661b7a562
qjkobe <513967622@qq.com>
2016-08-23 20:27:59 +0800

    MaxHeapify is amazing

---
+++ Chapter5/src/main/java/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2016/8/23.
+ */
+public class Main {
+
+    public void HireAssistant(int[] A){
+        int best = 0;
+        int n = A.length;
+        for(int i = 0; i < n; i++){
+
+        }
+    }
+}
---
+++ Chapter6/src/main/java/Sort.java
@@ -0,0 +1,85 @@
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2016/8/23.
+ */
+public class Sort {
+
+    int left(int i){
+        return i * 2 + 1;
+    }
+    int right(int i){
+        return i * 2 + 2;
+    }
+    int parent(int i){
+        return (i - 1) / 2;
+    }
+
+    public void MaxHeapify(int[] A, int i){
+        int l = left(i);
+        int r = right(i);
+        int heapSize = A.length;
+        int largest;
+        if(l <= heapSize && A[l] > A[i]){
+            largest = l;
+        }else{
+            largest = i;
+        }
+        if(r <= heapSize && A[r] > A[largest]){
+            largest = r;
+        }
+        if(largest != i){
+            int temp = A[i];
+            A[i] = A[largest];
+            A[largest] = temp;
+        }else{
+            return;
+        }
+        MaxHeapify(A, largest);
+    }
+
+    /**没有递归的最大堆排序
+     * @param A 堆数组
+     * @param i 需要排序的根节点
+     */
+    public void MaxHeapifyNoRecursive(int[] A, int i){
+        while(true){
+            int l = left(i);
+            int r = right(i);
+            int heapSize = A.length;
+            int largest;
+            if(l <= heapSize && A[l] > A[i]){
+                largest = l;
+            }else{
+                largest = i;
+            }
+            if(r <= heapSize && A[r] > A[largest]){
+                largest = r;
+            }
+            if(largest != i){
+                int temp = A[i];
+                A[i] = A[largest];
+                A[largest] = temp;
+            }else{
+                return;
+            }
+            i = largest;
+        }
+    }
+
+    @Test
+    public void TestMH(){
+        int[] A = new int[]{16, 4, 10, 14, 7, 9, 3, 2, 8, 1};
+        MaxHeapify(A, 1);
+        for(int x : A){
+            System.out.print(x + " ");
+        }
+        System.out.println();
+        int[] B = new int[]{16, 4, 10, 14, 7, 9, 3, 2, 8, 1};
+        MaxHeapifyNoRecursive(B, 1);
+        for(int x : B){
+            System.out.print(x + " ");
+        }
+    }
+
+}
