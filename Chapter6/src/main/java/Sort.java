0590e3a511030244fbdedaea3859948c57cb94ff
qjkobe <513967622@qq.com>
2016-08-24 22:29:09 +0800

    build max heap

--- Chapter6/src/main/java/Sort.java
+++ Chapter6/src/main/java/Sort.java
@@ -20,12 +20,12 @@ public class Sort {
         int r = right(i);
         int heapSize = A.length;
         int largest;
-        if(l <= heapSize && A[l] > A[i]){
+        if(l < heapSize && A[l] > A[i]){
             largest = l;
         }else{
             largest = i;
         }
-        if(r <= heapSize && A[r] > A[largest]){
+        if(r < heapSize && A[r] > A[largest]){
             largest = r;
         }
         if(largest != i){
@@ -48,12 +48,12 @@ public class Sort {
             int r = right(i);
             int heapSize = A.length;
             int largest;
-            if(l <= heapSize && A[l] > A[i]){
+            if(l < heapSize && A[l] > A[i]){
                 largest = l;
             }else{
                 largest = i;
             }
-            if(r <= heapSize && A[r] > A[largest]){
+            if(r < heapSize && A[r] > A[largest]){
                 largest = r;
             }
             if(largest != i){
@@ -64,6 +64,13 @@ public class Sort {
                 return;
             }
             i = largest;
+        }
+    }
+
+    public void BuildMaxHeap(int[] A){
+        int heapsize = A.length;
+        for(int i = (heapsize - 1) / 2; i >= 0; i--){
+            MaxHeapify(A, i);
         }
     }
 
@@ -78,6 +85,21 @@ public class Sort {
         int[] B = new int[]{16, 4, 10, 14, 7, 9, 3, 2, 8, 1};
         MaxHeapifyNoRecursive(B, 1);
         for(int x : B){
+            System.out.print(x + " ");
+        }
+        System.out.println();
+        int[] C = new int[]{4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
+        MaxHeapifyNoRecursive(C, 4);
+        for(int x : C){
+            System.out.print(x + " ");
+        }
+    }
+
+    @Test
+    public void TestBMH(){
+        int[] A = new int[]{4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
+        BuildMaxHeap(A);
+        for(int x : A){
             System.out.print(x + " ");
         }
     }
