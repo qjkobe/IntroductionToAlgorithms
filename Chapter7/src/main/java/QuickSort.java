22d6f8ca5b6f9efafeaaae647e25d81c65c15c58
qjkobe <513967622@qq.com>
2016-09-08 21:29:36 +0800

    finish quicksort

---
+++ Chapter7/src/main/java/QuickSort.java
@@ -0,0 +1,50 @@
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2016/9/8.
+ */
+public class QuickSort {
+
+    /**对子数组A[p...r]进行就地重排
+     * @param A
+     * @param p
+     * @param r
+     * @return
+     */
+    int Partition(int[] A, int p, int r){
+        int x = A[r];
+        int i = p - 1;
+        for(int j = p; j < r; j++){
+            if(A[j] <= x){
+                i++;
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
+        int temp = A[i + 1];
+        A[i + 1] = A[r];
+        A[r] = temp;
+        return i + 1;
+    }
+
+    void QuickSort(int[]A, int p, int r){
+        if(p < r){
+            int q = Partition(A, p, r);
+            QuickSort(A, p, q - 1);
+            QuickSort(A, q + 1, r);
+        }
+    }
+
+    /**
+     * 测试快速排序
+     */
+    @Test
+    public void TestQS(){
+        int[] A = new int[]{2, 8, 7, 1, 3, 5, 6, 4};
+        QuickSort(A, 0, 7);
+        for(int x : A){
+            System.out.println(x);
+        }
+    }
+}
