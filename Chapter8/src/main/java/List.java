406097e16583d26601a88bf1f046a8e81afe9755
qjkobe <513967622@qq.com>
2016-09-17 13:38:17 +0800

    Radix-sort

---
+++ Chapter8/src/main/java/List.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2016/9/17.
+ */
+public class List {
+
+    int val;
+    List next;
+
+    public List(int val) {
+        this.val = val;
+    }
+}
--- Chapter8/src/main/java/Sort.java
+++ Chapter8/src/main/java/Sort.java
@@ -22,7 +22,7 @@ public class Sort {
         for(int i = 1; i < k; i++){
             C[i] += C[i - 1];
         }
-        for(int i = 0; i < A.length; i++){
+        for(int i = A.length - 1; i >= 0; i--){
             B[C[A[i]] - 1] = A[i];
             C[A[i]]--;
         }
@@ -53,4 +53,105 @@ public class Sort {
             System.out.print(x + " ");
         }
     }
+
+    /**
+     * 获得数字的某位
+     * @param digit 数字
+     * @param i 1-d位
+     * @return 单位数字
+     */
+    public int getDigit(int digit, int i){
+        int result = 0;
+        for(int j = 0; j < i; j++){
+            result = digit % 10;
+            digit /= 10;
+        }
+        return result;
+    }
+
+    /**
+     * 计数排序变种，根据某一位进行排序，k不需要，因为每一位数字在0到9之间。
+     * @param A
+     * @param B
+     * @param index
+     */
+    public void CountingSort2(int[] A, int[] B, int index){
+        int[] C = new int[10];
+        for(int i = 0; i < A.length; i++){
+            C[getDigit(A[i], index)]++;
+        }
+        for(int i = 1; i < 10; i++){
+            C[i] += C[i - 1];
+        }
+        for(int i = A.length - 1; i >= 0; i--){
+            B[C[getDigit(A[i], index)] - 1] = A[i];
+            C[getDigit(A[i], index)]--;
+        }
+    }
+
+    @Test
+    public void TestCS2(){
+        int n = 0, i =0;
+        n = 8;
+        int[] A = new int[]{12, 25, 33, 10, 32, 43, 20, 13};
+        int[] B = new int[n];
+        CountingSort2(A, B, 2);
+        System.out.println("结果是：");
+        for(int x : B){
+            System.out.print(x + " ");
+        }
+    }
+
+    @Test
+    public void TestGD(){
+        System.out.println(getDigit(768, 2));
+    }
+
+    /**基数排序
+     * @param A 需要排序的数组，返回时已经排序
+     * @param d 最高位，最低位是1
+     */
+    public int[] RadixSort(int[] A, int d){
+        int[] B = A.clone();
+        for(int i = 1; i <= d; i++){
+            //调用某一稳定排序对A数组进行排序（基于i位）
+            CountingSort2(A, B, i);
+            A = B.clone();
+        }
+        return A;
+    }
+
+    /**
+     * 测试基数排序
+     */
+    @Test
+    public void TestRS(){
+        int[] A = new int[]{329, 457, 657, 839, 436, 720, 355};
+        A = RadixSort(A, 3);
+        for(int x : A){
+            System.out.print(x + " ");
+        }
+    }
+
+    /**
+     * 待完成，桶排序
+     * @param A
+     */
+    public void BucketSort(int[] A){
+        int n = A.length;
+        List[] B = new List[10];
+        for(int i = 0; i < n; i++){
+
+        }
+    }
+
+    /**
+     * 测试桶排序
+     */
+    @Test
+    public void TestBS(){
+        List[] B = new List[10];
+        System.out.println(B[1].val);
+    }
+
 }
